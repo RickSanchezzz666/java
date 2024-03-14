4aa91fda77d9c4949f040262f40bccb21efaa8ab
RickSanchezzz666 <lapiyktube@gmail.com>
2024-03-14 11:32:15 +0200

    lab4

---
+++ lab2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        var sailingVessel = new SailingVessel();
+        var submarine = new Submarine();
+
+        sailingVessel.PrepareToMovement();
+        sailingVessel.Move();
+
+        submarine.PrepareToMovement();
+        submarine.Move();
+    }
+}
